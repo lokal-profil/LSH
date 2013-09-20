70a96a1acca2521e5b923ed34f5e521e62e02af2
lokal-profil <lokal.profil@gmail.com>
2013-09-20 08:28:38 +0200

    New update functionality

--- py_Uploader.py
+++ py_Uploader.py
@@ -54,9 +54,9 @@ def upFiles(path, password=config.password, user=config.username, target=u'Uploa
     os.chdir(cwd) #so that same path structure can be used for next call
     flog.close()
 
-def updateInfo(path, password=config.password, user=config.username, target=u'Updated'):
+def updateInfoLocal(path, password=config.password, user=config.username, target=u'Updated'):
     '''
-    Rewrites the information on the given filepages. Essentially upFiles without the filetransfer
+    Overwrites the information on the given filepages. Essentially upFiles without the filetransfer
     '''
     cwd = os.getcwd()
     os.chdir(path)
@@ -90,3 +90,54 @@ def updateInfo(path, password=config.password, user=config.username, target=u'Up
     os.chdir(cwd) #so that same path structure can be used for next call
     flog.close()
     
+def updateInfoOnline(path, password=config.password, user=config.username, target=u'Updated', live=True):
+    '''
+    Rewrites the currently uploaded information, sends it to a modifier and uploads the new result
+    '''
+    cwd = os.getcwd()
+    os.chdir(path)
+    commons = setUp(password, user)
+    flog = codecs.open(u'¤updater.log','a', 'utf-8') #redirect print to logfile for the sake of PyCJWiki
+    comment = u'Updating information page due to improved algorithm for batch upload. See [[Commons:Batch_uploading/LSH]] for more info'
+    
+    #create targetdirectory if it doesn't exist
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    
+    files = os.listdir(u'.')
+    for f in files:
+        if f.endswith(u'.tif'):
+            print u'Working on: %s...' %f
+            infoFile = u'%s.txt' %f[:-4]
+            #get current version
+            oldInfo = commons.getText(u'File:%s' %f)
+            if not oldInfo:
+                flog.write(u'%s: Did not find file on Commons\n' %f)
+                continue
+            #create new info
+            newInfo = changeInfo(f, oldInfo)
+            if not newInfo:
+                flog.write(u'%s: Could not change info\n' %f)
+                continue
+            elif newInfo.strip()==oldInfo.strip():
+                flog.write(u'%s: No change needed\n' %f)
+                continue
+            #upload new info
+            if live:
+                result = commons.editText(u'File:%s' %f, newInfo, comment, minor=False, bot=True, userassert='exists', nocreate=True)
+                flog.write(u'%s\n' %result.decode('utf8'))
+                flog.flush()
+                #Move files
+                os.rename(f, os.path.join(target,f))
+                os.rename(infoFile, os.path.join(target,infoFile))
+            else:
+                flog.write(u'Update!: %s\n' % f)
+    commons.logout()
+    os.chdir(cwd) #so that same path structure can be used for next call
+    flog.close()
+    
+def changeInfo(fileName, oldInfo):
+    '''
+    Takes filename and oldInfo and changes into the new info. This method is expected to change each time
+    '''
+    return False
